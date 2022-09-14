b8d00d98296824d2dc67eb4f028c05c149c54e51
Tyler <93558042+tlsouth@users.noreply.github.com>
2022-09-14 12:13:01 -0400

    Add files via upload
    Lab 2 for Advanced Programming; added comments; extra credit options included

--- AminoQuiz.java
+++ AminoQuiz.java
@@ -7,23 +7,24 @@ public class AminoQuiz {
 
 public static void main(String[] args)
 {
-
-/**
- * this is JavaDoc notation	
- */
-	
+//allows generation of random numbers
 Random random = new Random();	
-	
+//empty string for adding the full name of the amino acid to
 String acid = "";
+//empty integer for storing index that will be used to store index of amino acid
 int index = 0;
+//casts type double on the system time of the machine so it can be used in calculation later
 Double start = (double) System.currentTimeMillis();
+//allows the program to read in numeric values input by the user
 Scanner quizTime = new Scanner (System.in);
+//asks the user how long they want the quiz to run (EXTRA CREDIT)
 System.out.println("How long would you like this quiz to run?");
+//sets double equal to the time input by the user for quiz time
 Double timer = quizTime.nextDouble();
+//converts stop time to seconds
 Double stop = start + timer * 1000;
-//Double elapsed = (System.currentTimeMillis() - start) * 0.001;
+//sets empty int variable to store the user's score
 int score = 0;
-//	name and single letter code are at the same index
 String[] SHORT_NAMES = 
 { "A","R", "N", "D", "C", "Q", "E",
 "G",  "H", "I", "L", "K", "M", "F", 
@@ -39,6 +40,7 @@ String[] FULL_NAMES =
 "serine","threonine","tryptophan", 
 "tyrosine", "valine"};
 
+//while loop that runs until the timer is up
 while(System.currentTimeMillis() <= stop)
 {
 //selects random integer between 0 and 19, which is the number of indices in the FULL_NAMES array
@@ -53,16 +55,19 @@ String code = System.console().readLine().toUpperCase();
 //if the user inputs the correct one letter code for the amino acid, the score is increased by 1
 if (code.equals(SHORT_NAMES[index])) 
 {
+	//casts long time on the time elapsed since quiz started in seconds and sets it to a variable to be returned to the user
 	Long elapsed = (long) ((System.currentTimeMillis() - start) * 0.001);
 	score ++;
-	System.out.println("Correct! Your score is: " + score + ". Time elapsed: " + elapsed + " seconds out of 30.");
+	//prints score and time elapsed since quiz started
+	System.out.println("Correct! Your score is: " + score + ". Time elapsed: " + elapsed + " seconds out of " + timer + '.');
 }
+//allows the user to type quit instead of amino acid code to terminate the program. not case sensitive since readLine is always reading upper case
 else if (code.equals("QUIT"))
 {
 	System.out.println("You have opted to quit. Your final score is " + score + ".");
 	return;
 }
-//if the user inputs the wrong one letter code, the program stops
+//if the user inputs the wrong one letter code, the program stops and returns a final score
 else
 {
 	System.out.println("Incorrect. Your final score is " + score + ".");
